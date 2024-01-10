784bd5e005852accd2c61f7c24ee7d841e91704a
Matthew Beke <mattbeke@gmail.com>
2024-01-09 19:56:29 -0500

    added bootstrap, jquery, and dayjs to html and began building out js

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+
+
+//listen for a click
+$(".weather_btn").on("click",()=>{
+    // get the value from the form
+    $(".current").append($(".city").val())
+    //get the coordinates
+
+    //pass the coordinates to the current weather
+    
+    //get the weather on the page
+});
