3bfa7235da891e83be74007c6f45b7bf8acff80d
Matthew Beke <mattbeke@gmail.com>
2024-01-09 21:25:02 -0500

    added feature to getCurrent to display current weather on the page

--- assets/script.js
+++ assets/script.js
@@ -3,11 +3,21 @@ var getCurrent = async(lat,lon)=>{
         lat,
         lon
     }`);
-    var response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=e88ce663b1df873686a4254bb12fef6f`);
+    var response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=e88ce663b1df873686a4254bb12fef6f`);
     //get the body out of the response
     var weather = await response.json();  
     //log the data
+    $(".current").append($(`<h1>${weather.name}</h1>`));
+    var myImage = $(`<img>`);
+    myImage.attr('src', `https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`);
+    $(".current").append(myImage);
+    $(".current").append($(`<p>Temperature: ${weather.main.temp}</p>`));
+    $(".current").append($(`<p>Wind: ${weather.wind.speed}</p>`));
+    $(".current").append($(`<p>Humidity: ${weather.main.humidity}</p>`));
     console.log(weather);
+    console.log(weather.name);
+    console.log(weather.main.temp);
+    console.log(weather.wind.speed);
 }
 
 var getCoordinates = async(city)=>{
@@ -24,7 +34,7 @@ console.log(city);
 //listen for a click
 $(".weather_btn").on("click",()=>{
     // get the value from the form
-    $(".current").append($(".city").val());
+    $(".current").empty();
     //get the coordinates
     getCoordinates($(".city").val());
     //pass the coordinates to the current weather
