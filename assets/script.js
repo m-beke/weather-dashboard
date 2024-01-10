ec5a19325626604546040a1644bdaac4b857daa2
Matthew Beke <mattbeke@gmail.com>
2024-01-09 20:44:14 -0500

    added function to get current weather from API

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,24 @@
+var getCurrent = async(lat,lon)=>{
+    console.log(`In current ${
+        lat,
+        lon
+    }`);
+    var response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=e88ce663b1df873686a4254bb12fef6f`);
+    //get the body out of the response
+    var weather = await response.json();  
+    //log the data
+    console.log(weather);
+}
+
 var getCoordinates = async(city)=>{
 console.log(city);
     var response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=10&appid=e88ce663b1df873686a4254bb12fef6f`);
     var data = await response.json();
-        console.log(data[0].lat);
-        console.log(data[0].lon);
+    // get our values
+    var lat = (data[0].lat);
+    var lon = (data[0].lon);
+
+    getCurrent(lat, lon);
 }
 
 //listen for a click
