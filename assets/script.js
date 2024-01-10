d392ae73bd9668ad68f8d64ce7bc6bbc13830840
Matthew Beke <mattbeke@gmail.com>
2024-01-09 22:19:02 -0500

    added function to get 5 day forecast and added css styling

--- assets/script.js
+++ assets/script.js
@@ -20,6 +20,28 @@ var getCurrent = async(lat,lon)=>{
     console.log(weather.wind.speed);
 }
 
+var getFuture = async(lat,lon)=>{
+    console.log(`Forecast ${
+        lat,
+        lon
+    }`);
+    var response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=e88ce663b1df873686a4254bb12fef6f`);
+    //get the body out of the response
+    var weather = await response.json();  
+    //log the data
+    $(".forecast").append($(`<h1>${list.dt}</h1>`));
+    var myImage = $(`<img>`);
+    myImage.attr('src', `https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`);
+    $(".forecast").append(myImage);
+    $(".forecast").append($(`<p>Temperature: ${weather.main.temp}</p>`));
+    $(".forecast").append($(`<p>Wind: ${weather.wind.speed}</p>`));
+    $(".forecast").append($(`<p>Humidity: ${weather.main.humidity}</p>`));
+    console.log(weather);
+    console.log(weather.name);
+    console.log(weather.main.temp);
+    console.log(weather.wind.speed);
+}
+
 var getCoordinates = async(city)=>{
 console.log(city);
     var response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=10&appid=e88ce663b1df873686a4254bb12fef6f`);
@@ -29,6 +51,7 @@ console.log(city);
     var lon = (data[0].lon);
 
     getCurrent(lat, lon);
+    getFuture(lat, lon);
 }
 
 //listen for a click
